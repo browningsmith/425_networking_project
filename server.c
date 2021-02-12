aae18f37fcc54e7a650476b941bdf6e8c041c0f5
Keith Smith <browningsmith@email.arizona.edu>
2021-02-12 12:44:52 -0700

    server able to bind on port 8080

--- server.c
+++ server.c
@@ -1,11 +1,16 @@
-#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8080
 
 int main(int argc, char** argv)
 {
     int socketFD; // Socket file descriptor
+    struct sockaddr_in serverAddress;
 
     // Create socket
     socketFD = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,6 +23,23 @@ int main(int argc, char** argv)
     {
         printf("Server created socket.\n");
     }
+
+    // Bind socket to PORT
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    // bind returns -1 on error
+    if (bind(socketFD, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
+    {
+        perror("Server unable to bind socket");
+        return -1;
+    }
+    else
+    {
+        printf("Server bound socket.\n");
+    }
+
 
     // Close socket
     if (close(socketFD)) // close returns -1 on error
