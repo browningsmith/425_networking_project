5ce5483fa71147a4bb9526aba52d8397cc4db80a
Keith Smith <browningsmith@email.arizona.edu>
2021-02-12 12:11:03 -0700

    Server and client can create and close sockets

--- server.c
+++ server.c
@@ -1,11 +1,34 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int main(int argc, char** argv)
 {
-    printf(
-        "Hello, I am a server!\n"
-        "Let me sing you the song of my people!\n"
-    );
+    int socketFD; // Socket file descriptor
+
+    // Create socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) // socket returns -1 on error
+    {
+        perror("Server unable to create socket");
+        return -1;
+    }
+    else
+    {
+        printf("Server created socket.\n");
+    }
+
+    // Close socket
+    if (close(socketFD)) // close returns -1 on error
+    {
+        perror("Server unable to close socket");
+    }
+    else
+    {
+        printf("Server closed socket.\n");
+    }
+
 
     return 0;
 }
