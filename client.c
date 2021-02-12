cc0ac88289ebcd502196980784b2d6ab081b046b
seanpcallahan <seanpcallahan@example.com>
2021-02-12 14:20:21 -0700

    started client input collection

--- client.c
+++ client.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <string.h>
+#define MAX_BUFFER 800
 
 int main(int argc, char** argv)
 {
+    char buffer[MAX_BUFFER];
+    uint32_t inputSize;
     int socketFD; // Socket file descriptor
 
     // Create socket
@@ -17,6 +21,22 @@ int main(int argc, char** argv)
     else
     {
         printf("Client created socket.\n");
+    }
+
+    // setting in inputing state
+    while(1){
+        // clearing the buffer each pass
+        bzero(buffer, sizeof(buffer)); 
+        printf("enter input: ");
+        // taking a line of user input
+        scanf("%s", buffer);
+        inputSize = strlen(buffer);
+        // send the data over to the server TODO
+
+
+        // if the input is zero close local program after sending
+        if(buffer[0] == '0') break;
+
     }
 
     // Close socket
