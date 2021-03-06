821a112f33856b6303d5c019cc3fc9adda6a86e7
Keith Smith <browningsmith@email.arizona.edu>
2021-03-06 10:45:23 -0700

    removed semicolons I accidentally put before if statement bodies

--- sproxy.c
+++ sproxy.c
@@ -214,7 +214,7 @@ int main(int argc, char** argv)
             // If input is ready on serverSocket, relay to clientSocket
             if (FD_ISSET(serverSocketFD, &socketSet))
             {
-                if (relay(serverSocketFD, clientSocketFD, buffer, BUFFER_LEN) < 0); // relay returns -1 on error
+                if (relay(serverSocketFD, clientSocketFD, buffer, BUFFER_LEN) < 0) // relay returns -1 on error
                 {
                     printf("Error relaying from server to client, one may have closed connection\n");
                     break; // Break out of loop to move on to close server and client
@@ -224,7 +224,7 @@ int main(int argc, char** argv)
             // If input is ready on clientSocket, relay to serverSocket
             if (FD_ISSET(clientSocketFD, &socketSet))
             {
-                if (relay(clientSocketFD, serverSocketFD, buffer, BUFFER_LEN) < 0); // relay returns -1 on error
+                if (relay(clientSocketFD, serverSocketFD, buffer, BUFFER_LEN) < 0) // relay returns -1 on error
                 {
                     printf("Error relaying from client to server, one may have closed connection\n");
                     break; // Break out of loop to move on to close server and client
