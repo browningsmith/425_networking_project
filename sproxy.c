26e9d012382a777356320496579ee3fa2b389d44
Keith Smith <browningsmith@email.arizona.edu>
2021-03-05 20:45:48 -0700

    I believe sproxy can now successfully receive a connection from, say telnet directly, and relay messages between telnet and telnet daemon

--- sproxy.c
+++ sproxy.c
@@ -19,20 +19,48 @@ Note:       This is the Server part of the program where the port number is
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUFFER_LEN 1024
 #define TELNET_PORT 23
+
+/*************************************
+ * max
+ * 
+ * Arguments: int a,b
+ * Retunrs: int
+ * 
+ * Returns the maximum of a and b
+ *************************************/
+int max(int a, int b);
+
+/*************************************
+ * relay
+ * 
+ * Arguments: int receiveFD, sendFD
+ *            char* buffer
+ * Returns: int
+ * 
+ * Reads the message on receiveFD, and
+ * writes it to sendFD
+ * 
+ * Returns -1 on error, 0 otherwise
+ *************************************/
+int relay(int receiveFD, int sendFD, void* buffer, int bufferSize);
 
 int main(int argc, char** argv)
 {
     int listenSocketFD, clientSocketFD, serverSocketFD; // Socket file descriptor
+    fd_set socketSet;
     int listenPort, serverPort;
-    size_t bytesRead;
     struct sockaddr_in listenAddress, serverAddress;
     struct sockaddr clientAddress;
     socklen_t clientAddressLength;
+    void* buffer = NULL;
 
     // Get port number to listen on from command line
     if (argc < 2)
@@ -65,10 +93,18 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    // listen to incoming connections
+    // set to listen to incoming connections
     if (listen(listenSocketFD, 5) < 0) // listen returns -1 on error
     {
         perror("sproxy unable to listen to port");
+        return -1;
+    }
+
+    // Allocate space for buffer
+    buffer = malloc(BUFFER_LEN);
+    if (buffer == NULL) // malloc returns NULL on error
+    {
+        perror("Unable to allocate space for buffer");
         return -1;
     }
 
@@ -129,7 +165,72 @@ int main(int argc, char** argv)
             continue; // move to accept new connection
         }
 
-        /* TODO  Add code to relay messages between server and client */
+        // Use select for data to be ready on both serverSocket and clientSocket
+        while (1)
+        {
+            // Reset socketSet
+            FD_ZERO(&socketSet); // zero out socketSet
+            FD_SET(serverSocketFD, &socketSet); // add server socket
+            FD_SET(clientSocketFD, &socketSet); // add client socket
+
+            // Wait indefinitely for input to be available using select
+            if(
+                select(
+                    max(serverSocketFD, clientSocketFD) + 1,
+                    &socketSet,
+                    NULL,
+                    NULL,
+                    NULL
+                ) < 0 // select returns -1 on error
+            )
+            {
+                perror("sproxy unable to use select to wait for input");
+
+                // Close server socket, so it doesn't stay open
+                if (close(serverSocketFD)) // close returns -1 on error
+                {
+                    perror("sproxy unable to properly close server socket");
+                }
+                else
+                {
+                    printf("sproxy closed connection to server\n");
+                }
+
+                // Close client socket, so it doesn't stay open
+                if (close(clientSocketFD)) // close returns -1 on error
+                {
+                    perror("sproxy unable to properly close client socket");
+                }
+                else
+                {
+                    printf("sproxy closed connection to client\n");
+                }
+
+                return -1;
+            }
+
+            ssize_t bytesRead;
+
+            // If input is ready on serverSocket, relay to clientSocket
+            if (FD_ISSET(serverSocketFD, &socketSet))
+            {
+                if (relay(serverSocketFD, clientSocketFD, buffer, BUFFER_LEN) < 0); // relay returns -1 on error
+
+                printf("Error relaying from server to client, one may have closed connection\n");
+
+                break; // Break out of loop to move on to close server and client
+            }
+
+            // If input is ready on clientSocket, relay to serverSocket
+            if (FD_ISSET(clientSocketFD, &socketSet))
+            {
+                if (relay(clientSocketFD, serverSocketFD, buffer, BUFFER_LEN) < 0); // relay returns -1 on error
+
+                printf("Error relaying from client to server, one may have closed connection\n");
+
+                break; // Break out of loop to move on to close server and client
+            }
+        }
 
         // Close server socket
         if (close(serverSocketFD)) // close returns -1 on error
@@ -158,5 +259,41 @@ int main(int argc, char** argv)
         perror("sproxy unable to close listen socket");
     }
 
+    // free buffer
+    free(buffer);
+
     return 0;
 }
+
+int relay(int receiveFD, int sendFD, void* buffer, int bufferSize)
+{
+    // Read from receiveFD into buffer
+    ssize_t bytesRead = recv(receiveFD, buffer, bufferSize, 0);
+    if (bytesRead < 1) // Returns 0 on closed connection, -1 on error
+    {
+        printf("Unable to receive bytes, either connection closed or error\n");
+        return -1;
+    }
+
+    // Write from buffer to sendFD
+    ssize_t bytesSent = send(sendFD, buffer, bytesRead, 0);
+    if (bytesSent < 1) // Returns -1 on error
+    {
+        printf("Unable to send bytes");
+        return -1;
+    }
+
+    return 0;
+}
+
+int max(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+
+    return b;
+}
+
+
