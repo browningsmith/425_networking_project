f35391f3a72c0f62a73d16b891d419d0986c36f1
Keith Smith <browningsmith@email.arizona.edu>
2021-03-05 17:38:19 -0700

    Copied files from Milestone1 into their own folder

---
+++ Milestone1/client.c
@@ -0,0 +1,93 @@
+/*
+Authors:    Keith Smith, Sean Callahan
+Assignment: Mobile TCP Proxy, Milestone 1
+file:       client.c
+Class:      425
+Due Date:   02/16/2021
+
+Note:       This is the client part of the program where the IP and port number are
+            given as command line arguments. As far as the design goes the payload is
+            constructed as a string array without a termination character and the first
+            4 bytes is the length of the broadcast string
+*/
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define MAX_BUFFER 1024
+
+int main(int argc, char** argv)
+{
+    char buffer[MAX_BUFFER + 2]; // Include two extra bytes, one for the newline or EOF character, and one for the null terminator.
+                                 // These will not be transmitted to the server
+    char payload[MAX_BUFFER + 4];
+    uint32_t inputSize;
+    struct sockaddr_in serverAddress, cli;
+    int socketFD; // Socket file descriptor
+    int connFD;
+    int port;
+    //int ip;
+    int i;
+
+    // get port number and IP from command line
+    if (argc < 3)
+    {
+        printf(
+            "ERROR: No port/IP specified!\n"
+            "Usage: ./client IP portNumber\n"
+        );
+        return -1;
+    }
+    port = atoi(argv[2]);
+
+    // Create socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) // socket returns -1 on error
+    {
+        perror("Client unable to create socket");
+        return -1;
+    }
+
+    // Bind socket to port and IP
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
+    serverAddress.sin_port = htons(port);
+
+    // connect to a remote server on a certain IP and port
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0){
+        perror("Client unable to connect to server");
+        return -1;
+    }
+
+    // reading user input till ctrl+d
+    while(fgets(buffer, MAX_BUFFER + 2, stdin) != NULL){
+        // technically strlen should not be used in this assignment but with fgets it works
+        inputSize = strlen(buffer) - 1; // Disregard newline or EOF character
+        // setting the first elem to be the size
+        ((uint32_t *)payload)[0] = inputSize;
+        // copying the input into the payload
+        for(int i=0; i<inputSize; i++){
+            payload[i + 4] = buffer[i];
+        }
+        // send the payload to the server
+        write(socketFD, payload, inputSize + sizeof(uint32_t));
+        // clean up arrays for no buggies
+        bzero(buffer, sizeof(buffer));
+        bzero(payload, sizeof(payload));
+    }
+
+    // Close socket
+    if (close(socketFD)) // close returns -1 on error
+    {
+        perror("Client unable to close socket");
+    }
+    return 0;
+}
---
+++ Milestone1/server.c
@@ -0,0 +1,122 @@
+/*
+Authors:    Keith Smith, Sean Callahan
+Assignment: Mobile TCP Proxy, Milestone 1
+File:       server.c
+Class:      425
+Due Date:   02/16/2021
+
+Note:       This is the Server part of the program where the port number is
+            given as command line argument.. As far as the design goes the payload is
+            constructed as a string array without a termination character and the first
+            4 bytes is the length of the broadcast string. The size is read first then
+            printed to the console followed by the message in the payload
+*/
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define BUFFLEN 1024
+
+int main(int argc, char** argv)
+{
+    int serverSocketFD, clientSocketFD; // Socket file descriptor
+    int port;
+    size_t bytesRead;
+    struct sockaddr_in serverAddress;
+    struct sockaddr clientAddress;
+    socklen_t clientAddressLength;
+    uint32_t messageLength;
+    void* receiveBuffer = malloc(BUFFLEN);
+
+    // Get port number from command line
+    if (argc < 2)
+    {
+        printf(
+            "ERROR: No port specified!\n"
+            "Usage: ./server portNumber\n"
+        );
+        return -1;
+    }
+    port = atoi(argv[1]);
+
+    // Create server socket
+    serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocketFD < 0) // socket returns -1 on error
+    {
+        perror("Server unable to create socket");
+        return -1;
+    }
+
+    // Bind server to port
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    // bind returns -1 on error
+    if (bind(serverSocketFD, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
+    {
+        perror("Server unable to bind socket to port");
+        return -1;
+    }
+
+    // listen to incoming connections
+    if (listen(serverSocketFD, 5) < 0) // listen returns -1 on error
+    {
+        perror("Server unable to listen to port");
+        return -1;
+    }
+    
+    // accepting the connected client
+    clientSocketFD = accept(serverSocketFD, &clientAddress, &clientAddressLength);
+    if (clientSocketFD < -1) // accept returns -1 on error
+    {
+        perror("Server unable to receive connection from client");
+        return -1;
+    }
+
+    // reading a message
+    messageLength = 1; // As long as messageLength is greater than 0, continue trying to read
+    while (messageLength > 0)
+    {
+        // Read first 4 bytes, the length of message
+        bytesRead = recv(clientSocketFD, receiveBuffer, 4, 0);
+        if (bytesRead < 1) // recv returns -1 on error, or 0 on close
+        {
+            break;
+        }
+        
+        messageLength = *((uint32_t*) receiveBuffer);
+        printf("Payload length: %i\n", messageLength);
+        
+        // Read payload
+        bytesRead = recv(clientSocketFD, receiveBuffer, messageLength, 0);
+        if (bytesRead < 1) // recv returns -1 on error, or 0 on close
+        {
+            break;
+        }
+        
+        // Write payload to stdout. Using write since buffer doesn't have a null terminating character
+        write(STDOUT_FILENO, receiveBuffer, messageLength);
+        printf("\n");
+    }
+
+    // Close client socket
+    if (close(clientSocketFD)) // close returns -1 on error
+    {
+        perror("Server unable to close client socket");
+    }
+
+    // Close server socket
+    if (close(serverSocketFD)) // close returns -1 on error
+    {
+        perror("Server unable to close server socket");
+    }
+
+    // Free receiveBuffer
+    free(receiveBuffer);
+
+    return 0;
+}
