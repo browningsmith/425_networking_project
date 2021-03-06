7eb73e44dd5f49ee5a6d7fec5b2bed0973cc0056
Keith Smith <browningsmith@email.arizona.edu>
2021-03-05 18:08:52 -0700

    Updated terminology in sproxy to refer to the listening socket as listenSocket/listenAddress instead of serverSocket/serverAddress. Removed message receiving code, will replace with implementation of select

--- sproxy.c
+++ sproxy.c
@@ -20,105 +20,73 @@ Note:       This is the Server part of the program where the port number is
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUFFLEN 1024
-
 int main(int argc, char** argv)
 {
-    int serverSocketFD, clientSocketFD; // Socket file descriptor
-    int port;
+    int listenSocketFD, clientSocketFD; // Socket file descriptor
+    int listenPort;
     size_t bytesRead;
-    struct sockaddr_in serverAddress;
+    struct sockaddr_in listenAddress;
     struct sockaddr clientAddress;
     socklen_t clientAddressLength;
-    uint32_t messageLength;
-    void* receiveBuffer = malloc(BUFFLEN);
 
-    // Get port number from command line
+    // Get port number to listen on from command line
     if (argc < 2)
     {
         printf(
             "ERROR: No port specified!\n"
-            "Usage: ./server portNumber\n"
+            "Usage: ./sproxy portNumber\n"
         );
         return -1;
     }
-    port = atoi(argv[1]);
+    listenPort = atoi(argv[1]);
 
-    // Create server socket
-    serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocketFD < 0) // socket returns -1 on error
+    // Create listen socket
+    listenSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocketFD < 0) // socket returns -1 on error
     {
-        perror("Server unable to create socket");
+        perror("sproxy unable to create listen socket");
         return -1;
     }
 
-    // Bind server to port
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(port);
+    // Bind listen socket to port
+    listenAddress.sin_family = AF_INET;
+    listenAddress.sin_addr.s_addr = INADDR_ANY;
+    listenAddress.sin_port = htons(listenPort);
 
     // bind returns -1 on error
-    if (bind(serverSocketFD, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0)
+    if (bind(listenSocketFD, (struct sockaddr*) &listenAddress, sizeof(listenAddress)) < 0)
     {
-        perror("Server unable to bind socket to port");
+        perror("sproxy unable to bind listen socket to port");
         return -1;
     }
 
     // listen to incoming connections
-    if (listen(serverSocketFD, 5) < 0) // listen returns -1 on error
+    if (listen(listenSocketFD, 5) < 0) // listen returns -1 on error
     {
-        perror("Server unable to listen to port");
+        perror("sproxy unable to listen to port");
         return -1;
     }
     
     // accepting the connected client
-    clientSocketFD = accept(serverSocketFD, &clientAddress, &clientAddressLength);
+    clientSocketFD = accept(listenSocketFD, &clientAddress, &clientAddressLength);
     if (clientSocketFD < -1) // accept returns -1 on error
     {
-        perror("Server unable to receive connection from client");
+        perror("sproxy to receive connection from client");
         return -1;
     }
-
-    // reading a message
-    messageLength = 1; // As long as messageLength is greater than 0, continue trying to read
-    while (messageLength > 0)
-    {
-        // Read first 4 bytes, the length of message
-        bytesRead = recv(clientSocketFD, receiveBuffer, 4, 0);
-        if (bytesRead < 1) // recv returns -1 on error, or 0 on close
-        {
-            break;
-        }
-        
-        messageLength = *((uint32_t*) receiveBuffer);
-        printf("Payload length: %i\n", messageLength);
-        
-        // Read payload
-        bytesRead = recv(clientSocketFD, receiveBuffer, messageLength, 0);
-        if (bytesRead < 1) // recv returns -1 on error, or 0 on close
-        {
-            break;
-        }
-        
-        // Write payload to stdout. Using write since buffer doesn't have a null terminating character
-        write(STDOUT_FILENO, receiveBuffer, messageLength);
-        printf("\n");
-    }
+    printf("sproxy accepted connection from client!\n");
 
     // Close client socket
     if (close(clientSocketFD)) // close returns -1 on error
     {
-        perror("Server unable to close client socket");
+        perror("sproxy unable to close client socket");
     }
 
-    // Close server socket
-    if (close(serverSocketFD)) // close returns -1 on error
+    // Close listen socket
+    if (close(listenSocketFD)) // close returns -1 on error
     {
-        perror("Server unable to close server socket");
+        perror("sproxy unable to close server socket");
     }
-
-    // Free receiveBuffer
-    free(receiveBuffer);
 
     return 0;
 }
